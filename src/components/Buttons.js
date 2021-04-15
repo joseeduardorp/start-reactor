7da2db495e7cd4bb14c48bbe597bee8d2b69d20d
José Eduardo <joseeduardorperes@gmail.com>
2021-04-15 11:59:52 -0300

    add button panel component and modal component

---
+++ src/components/Buttons.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Buttons() {
+  const lista = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  const buttons = lista.map((index) => <button key={index} className="button" />);
+
+  return (
+    <div className="Buttons">
+      {buttons}
+    </div>
+  );
+}
---
+++ src/components/Modal.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Modal() {
+  return (
+    <div className="Modal">
+      <h2>Press Enter to Start Game</h2>
+    </div>
+  );
+}
