b140460c4043a6a27b05ce7d1abb15f0e9a45524
José Eduardo <joseeduardorperes@gmail.com>
2021-04-15 11:58:12 -0300

    button panel interface complete and add modal

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CurrentPosition from './CurrentPosition.js';
-import Display from './Display.js';
 import Panel from './Panel.js';
+import Modal from './Modal.js';
+import Display from './Display.js';
+import Buttons from './Buttons.js';
 
 export default function Game() {
+  const [start, setStart] = useState(true);
+
+  const handleKeyPress = (event) => {
+    return event.key === "Enter" && setStart(false);
+  }
+
   return (
-    <div className="Game">
+    <div className="Game" tabIndex="-1" onKeyPress={handleKeyPress}>
+      {start && <Modal />}
+
       <Panel>
         <CurrentPosition />
         <Display />
       </Panel>
 
       <Panel>
-        <h2>Painel 2</h2>
+        <CurrentPosition />
+        <Buttons />
       </Panel>
     </div>
   );
