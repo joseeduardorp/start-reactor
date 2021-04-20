765d7f253034256799a9d9718692f38ea2a4f18f
José Eduardo <joseeduardorperes@gmail.com>
2021-04-20 16:28:55 -0300

    added background image and started game development

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Buttons() {
   const lista = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-  const buttons = lista.map((index) => <button key={index} className="button" />);
+  const buttons = lista.map((index) => <button key={index} className="button" onClick={() => alert(`botão nº${index}`)} />);
 
   return (
     <div className="Buttons">
--- src/components/Game.js
+++ src/components/Game.js
@@ -6,23 +6,67 @@ import Display from './Display.js';
 import Buttons from './Buttons.js';
 
 export default function Game() {
-  const [start, setStart] = useState(true);
+  const [start, setStart] = useState(false);
+  const [randomList, setRandomList] = useState([]);
+  const [currentPosition, setCurrentPosition] = useState([4]);
+
+  // ==================================================================================
+  // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  // ==================================================================================
+  const randomizeNumber = () => {
+    let randomNumbersList = [];
+
+    for (let i = 0; i < 5; i++) {
+      let randomNumber = Math.floor(Math.random() * 9);
+      randomNumbersList.push(randomNumber);
+    }
+
+    return randomNumbersList;
+  }
 
   const handleKeyPress = (event) => {
-    return event.key === "Enter" && setStart(false);
+    return event.key === "Enter" && setStart(true);
+  }
+
+  const verify = (index, currentPosition) => {
+    for (let i = 0; i < currentPosition.length; i++) {
+      if (index === currentPosition[i]) return true;
+    }
   }
 
   useEffect(() => {
     window.document.body.addEventListener('keypress', handleKeyPress);
+
+    setRandomList(randomizeNumber());
   }, []);
+
+  // ==================================================================================
+  // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  // ==================================================================================
+
+  console.log("teste: " + randomList);
+  console.log("teste: " + currentPosition);
+
+  const lista = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  const displayButtons = lista.map((index) => {
+    let isOn = "DisplayButton";
+
+    let teste = verify(index, currentPosition);
+    
+    if (start && teste) isOn += " On";
+
+    return (
+      <div key={index} className={isOn} />
+    )
+  });
 
   return (
     <div className="Game">
-      {start && <Modal />}
+      {!start && <Modal />}
 
       <Panel>
         <CurrentPosition />
-        <Display />
+        <Display isStart={start} randomList={randomList} displayButtons={displayButtons} />
       </Panel>
 
       <Panel>
