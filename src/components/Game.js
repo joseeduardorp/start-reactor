7ed49ad43f0d58bb754910a63d005e0c4346c16c
José Eduardo <joseeduardorperes@gmail.com>
2021-04-30 16:32:07 -0300

    nothing important

--- src/components/Game.js
+++ src/components/Game.js
@@ -6,16 +6,31 @@ import Display from './Display.js';
 import Buttons from './Buttons.js';
 
 export default function Game() {
-  const [start, setStart] = useState(false);
+  const [start, setStart] = useState(true); // mudar para false
+  const [randomNumberList, setRandomNumberList] = useState([]);
 
-  const handleKeyPress = (event) => {
+  function randomizeNumberList() {
+    let randomNumberList = [];
+
+    for (let i = 0; i < 5; i++) {
+      randomNumberList.push(Math.floor(Math.random() * 9));
+    }
+
+    return setRandomNumberList(randomNumberList);
+  }
+
+  function handleKeyPress(event) {
     return event.key === "Enter" && setStart(true);
   }
 
   useEffect(() => {
     window.document.body.addEventListener('keypress', handleKeyPress);
+
+    randomizeNumberList();
   }, []);
 
+  console.log(randomNumberList);
+  
   return (
     <div className="Game">
       {!start && <Modal />}
